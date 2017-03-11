13582b205f8eb6573574235f14015c49991b0389
mjschock <mschock@ocf.berkeley.edu>
2017-03-10 23:48:47 -0800

    progress

--- game_agent.py
+++ game_agent.py
@@ -6,7 +6,6 @@ augment the test suite with your own test cases to further test your code.
 You must test your agent's strength against a set of agents with known
 relative strength using tournament.py and include the results in your report.
 """
-import random
 import math
 
 
@@ -26,6 +25,27 @@ def get_distance(distance_metric, p, q, a = 0.5):
         return abs(p[0] - q[0]) + abs(p[1] - q[1])
     elif distance_metric == 'akritean':
         return euclidean(p, q)*(1-a) + manhattan(p, q)*a
+
+def actions(state):
+    return state.get_legal_moves(state.active_player)
+
+def terminal_test(state, depth):
+    return depth == 0 or len(actions(state)) == 0
+
+def result(state, action):
+    return state.forecast_move(action)
+
+def heuristic_value(game, depth, maximizing_player, score_fn):
+    best_score = float("-inf") if maximizing_player else float("inf")
+    best_move = (-1, -1)
+
+    if len(game.get_legal_moves(game.active_player)) == 0:
+        return best_score, best_move
+    elif depth == 0:
+        if maximizing_player:
+            return score_fn(game, game.active_player), best_move
+        else:
+            return score_fn(game, game.inactive_player), best_move
 
 def custom_score(game, player):
     """Calculate the heuristic value of a game state from the point of view
@@ -51,13 +71,8 @@ def custom_score(game, player):
     """
 
     # TODO: finish this function!
-    if game.is_loser(player):
-        return float("-inf")
 
-    if game.is_winner(player):
-        return float("inf")
-
-    def get_score(game, player):
+    def get_score(game, player, dig = False):
         opponent = game.get_opponent(player)
         player_location = game.get_player_location(player)
         opponent_location = game.get_player_location(opponent)
@@ -70,36 +85,46 @@ def custom_score(game, player):
         opponent_distance_to_middle = get_distance('manhattan', opponent_location, middle_position)
         diff_legal_moves = num_player_legal_moves - num_opponent_legal_moves
         diff_distances_to_middle = player_distance_to_middle - opponent_distance_to_middle
-        score = float(diff_legal_moves - num_blank_spaces + distance_between_players + diff_distances_to_middle) #52.14%, manhattan
+
+        if game.is_loser(player):
+            return float("-inf")
+        if game.is_winner(player):
+            return float("inf")
+
+        # score = float(distance_between_players) # 5%
+        # score = float(-distance_between_players) # 37.50% manhattan
+        # score = float(-distance_between_players) # 32.50% euclidean
+        # score = float(-distance_between_players) # 32.50% akritean
+        # score = float(diff_legal_moves - distance_between_players) # 10% manhattan
+        # score = float(diff_legal_moves) # 5%
+        # score = float(num_player_legal_moves - 2*num_opponent_legal_moves) # 17.50%
+        # score = float(num_player_legal_moves - 3*num_opponent_legal_moves) # 37.50%
+        # score = float(num_player_legal_moves - 4*num_opponent_legal_moves) # 25.00%
+        # score = float(num_player_legal_moves - math.pi * num_opponent_legal_moves) # 45.00%
+        # score = float(num_player_legal_moves - math.pi * num_opponent_legal_moves - distance_between_players) # 20.00%
+        # score = float(num_player_legal_moves - math.pi * num_opponent_legal_moves - 2 * distance_between_players) # 10.00%
+        # score = float(num_player_legal_moves - math.pi * num_opponent_legal_moves - 0.0001 * distance_between_players) # 22.50%
+        # score = float(num_player_legal_moves - math.pi * num_opponent_legal_moves + distance_between_players) # 20%
+        # score = float(num_player_legal_moves - math.pi * (num_opponent_legal_moves + distance_between_players)) # 35.00%
+        # score = float(num_player_legal_moves - math.pi * (num_opponent_legal_moves + 0.5 * distance_between_players)) # 25.00%
+        # score = float(num_player_legal_moves - (math.pi * num_opponent_legal_moves) - (math.e * distance_between_players)) # 17.50%
+        # score = float(num_player_legal_moves - (math.pi * num_opponent_legal_moves) - (math.tau * distance_between_players)) # 15.00%
+        # score = float(num_player_legal_moves - math.tau * num_opponent_legal_moves) # 30.00%
+        # score = float(num_player_legal_moves - math.pi * num_opponent_legal_moves - num_blank_spaces) #60%
+        # score = float(num_player_legal_moves - math.pi * num_opponent_legal_moves - 2 * num_blank_spaces) #75%
+        # score = float(num_player_legal_moves - math.pi * (num_opponent_legal_moves - num_blank_spaces)) #0%
+        # score = float(num_player_legal_moves - math.pi * num_opponent_legal_moves - 3 * num_blank_spaces) #80.00%
+        # score = float(num_player_legal_moves - math.pi * num_opponent_legal_moves - 6 * num_blank_spaces) #67.50%
+        # score = float(num_player_legal_moves - math.pi * num_opponent_legal_moves - 4 * num_blank_spaces) #75.00%
+        # score = float(num_player_legal_moves - math.pi * (num_opponent_legal_moves + num_blank_spaces)) #77.50%
+        # score = float(num_player_legal_moves - 3 * (num_opponent_legal_moves + num_blank_spaces)) #67.50%
+        # score = float(num_player_legal_moves - math.pi * num_opponent_legal_moves - math.e * num_blank_spaces) #72.50%
+        score = float(num_player_legal_moves - math.pi * num_opponent_legal_moves - 3 * num_blank_spaces) #67.50%
         return score
 
-    score = get_score(game, player)
-    # opponent = game.get_opponent(player)
-
-    # current_player_legal_moves = game.get_legal_moves(player)
-
-    # for move in current_player_legal_moves:
-    #     next_game = game.forecast_move(move)
-    #     score = score + get_score(next_game, player) # 48.57%
-
-    # opponent_player_legal_moves = game.get_legal_moves(opponent)
-
-    # for move in opponent_player_legal_moves:
-    #     next_game = game.forecast_move(move)
-    #     score = score - get_score(next_game, player)
+    score = get_score(game, player, True)
 
     return score
-
-def actions(state):
-    return state.get_legal_moves(state.active_player)
-
-def terminal_test(state, depth):
-    # _utility = state.utility(state, state.active_player)
-    # return depth == 0 or state.utility(state.active_player) != 0
-    return depth == 0 or len(actions(state)) == 0
-
-def result(state, action):
-    return state.forecast_move(action)
 
 class CustomPlayer:
     """Game-playing agent that chooses a move using your evaluation function
@@ -179,17 +204,17 @@ class CustomPlayer:
         self.time_left = time_left
 
         # TODO: finish this function!
+        best_score = float('-inf')
+        best_move = (-1, -1)
 
         # Perform any required initializations, including selecting an initial
         # move from the game board (i.e., an opening book), or returning
         # immediately if there are no legal moves
         if len(legal_moves) == 0:
-            return (-1, -1)
+            return best_move
 
-        active_player_location = game.get_player_location(game.active_player)
-        inactive_player_location = game.get_player_location(game.inactive_player)
-
-        best_move = legal_moves[0] # if the middle is open, try that?
+        # active_player_location = game.get_player_location(game.active_player)
+        # inactive_player_location = game.get_player_location(game.inactive_player)
 
         # if active_player_location is None and inactive_player_location is None:
         #     return (round(game.width / 2), round(game.height / 2))
@@ -203,25 +228,31 @@ class CustomPlayer:
         #             best_move = legal_move
         #     return best_move
 
+        def run_method(best_score, best_move, depth):
+            if self.method == 'minimax':
+                next_best_score, next_best_move = self.minimax(game, depth)
+            elif self.method == 'alphabeta':
+                next_best_score, next_best_move = self.alphabeta(game, depth)
+
+            if next_best_score >= best_score:
+                best_score = next_best_score
+                best_move = next_best_move
+
+            return best_score, best_move
+
         try:
             # The search method call (alpha beta or minimax) should happen in
             # here in order to avoid timeout. The try/except block will
             # automatically catch the exception raised by the search method
             # when the timer gets close to expiring
-            def run_method(depth):
-                if self.method == 'minimax':
-                    _, best_move = self.minimax(game, depth)
-                elif self.method == 'alphabeta':
-                    _, best_move = self.alphabeta(game, depth)
-
             if self.iterative:
                 # depth = 0
                 depth = -1
                 while True:
                     depth += 1
-                    run_method(depth)
+                    best_score, best_move = run_method(best_score, best_move, depth)
             else:
-                run_method(self.search_depth)
+                best_score, best_move = run_method(best_score, best_move, self.search_depth)
 
         except Timeout:
             # Handle any actions required at timeout, if necessary
@@ -265,48 +296,40 @@ class CustomPlayer:
             raise Timeout()
 
         # TODO: finish this function!
+        best_score = float("-inf") if maximizing_player else float("inf")
         best_move = (-1, -1)
 
-        if len(game.get_legal_moves(game.active_player)) == 0:
-            if maximizing_player:
-                return game.utility(game.active_player), best_move
-            else:
-                return game.utility(game.inactive_player), best_move
-        elif depth == 0:
-            if maximizing_player:
-                return self.score(game, game.active_player), best_move
-            else:
-                return self.score(game, game.inactive_player), best_move
+        if terminal_test(game, depth):
+            return heuristic_value(game, depth, maximizing_player, self.score)
 
-        # if maximizing_player:
-        #     best_score = float("-inf")
-        #     for action in actions(game):
-        #         next_game_state = result(game, action)
-        #         next_best_score, _ = self.minimax(next_game_state, depth - 1, False)
-        #         if next_best_score > best_score:
-        #             best_score = next_best_score
-        #             best_move = action
-        #     return best_score, best_move
-        # else:
-        #     best_score = float("inf")
-        #     for action in actions(game):
-        #         next_game_state = result(game, action)
-        #         next_best_score, _ = self.minimax(next_game_state, depth - 1, True)
-        #         if next_best_score < best_score:
-        #             best_score = next_best_score
-        #             best_move = action
-        #     return best_score, best_move
+        if maximizing_player:
+            best_score = float("-inf")
+            for action in actions(game):
+                next_game_state = result(game, action)
+                next_best_score, _ = self.minimax(next_game_state, depth - 1, False)
+                if next_best_score > best_score:
+                    best_score = next_best_score
+                    best_move = action
+            return best_score, best_move
+        else:
+            best_score = float("inf")
+            for action in actions(game):
+                next_game_state = result(game, action)
+                next_best_score, _ = self.minimax(next_game_state, depth - 1, True)
+                if next_best_score < best_score:
+                    best_score = next_best_score
+                    best_move = action
+            return best_score, best_move
 
-        best_score = float("-inf") if maximizing_player else float("inf")
-        for action in actions(game):
-            next_game_state = result(game, action)
-            next_layer = False if maximizing_player else True
-            next_best_score, _ = self.minimax(next_game_state, depth - 1, next_layer)
-            new_maximizing_best_score = maximizing_player and next_best_score > best_score
-            new_minimizing_best_score = maximizing_player is False and next_best_score < best_score
-            if new_maximizing_best_score or new_minimizing_best_score:
-                best_score = next_best_score
-                best_move = action
+        # for action in actions(game):
+        #     next_game_state = result(game, action)
+        #     next_layer = False if maximizing_player else True
+        #     next_best_score, _ = self.minimax(next_game_state, depth - 1, next_layer)
+        #     new_maximizing_best_score = maximizing_player and next_best_score > best_score
+        #     new_minimizing_best_score = maximizing_player is False and next_best_score < best_score
+        #     if new_maximizing_best_score or new_minimizing_best_score:
+        #         best_score = next_best_score
+        #         best_move = action
 
         return best_score, best_move
 
@@ -352,27 +375,18 @@ class CustomPlayer:
             raise Timeout()
 
         # TODO: finish this function!
+        best_score = float("-inf") if maximizing_player else float("inf")
         best_move = (-1, -1)
 
-        if len(game.get_legal_moves(game.active_player)) == 0:
-            if maximizing_player:
-                return game.utility(game.active_player), best_move
-            else:
-                return game.utility(game.inactive_player), best_move
-        elif depth == 0:
-            if maximizing_player:
-                return self.score(game, game.active_player), best_move
-            else:
-                return self.score(game, game.inactive_player), best_move
-
-        best_score = float("-inf") if maximizing_player else float("inf")
+        if terminal_test(game, depth):
+            return heuristic_value(game, depth, maximizing_player, self.score)
 
         for action in actions(game):
             next_game_state = result(game, action)
             next_layer = False if maximizing_player else True
             next_best_score, _ = self.alphabeta(next_game_state, depth - 1, alpha, beta, next_layer)
-            new_maximizing_best_score = maximizing_player and next_best_score > best_score
-            new_minimizing_best_score = maximizing_player is False and next_best_score < best_score
+            new_maximizing_best_score = maximizing_player and (next_best_score > best_score)
+            new_minimizing_best_score = maximizing_player is False and (next_best_score < best_score)
             if new_maximizing_best_score or new_minimizing_best_score:
                 best_score = next_best_score
                 best_move = action
@@ -384,25 +398,5 @@ class CustomPlayer:
                 if best_score <= alpha:
                     return best_score, best_move
                 beta = min(beta, best_score)
-
-        # best_score = float("-inf") if maximizing_player else float("inf")
-        # for action in actions(game):
-        #     next_game_state = result(game, action)
-        #     next_layer = False if maximizing_player else True
-        #     next_best_score, _ = self.alphabeta(next_game_state, depth - 1, alpha, beta, next_layer)
-        #     new_maximizing_best_score = maximizing_player and next_best_score > best_score
-        #     new_minimizing_best_score = maximizing_player is False and next_best_score < best_score
-        #     if new_maximizing_best_score or new_minimizing_best_score:
-        #         # best_score = self.score(game, game.inactive_player) + next_best_score
-        #         best_score = next_best_score # wtf!
-        #         best_move = action
-        #     if maximizing_player:
-        #         if best_score >= beta:
-        #             return best_score, best_move
-        #         alpha = max(alpha, best_score)
-        #     else:
-        #         if best_score <= alpha:
-        #             return best_score, best_move
-        #         beta = min(beta, best_score)
 
         return best_score, best_move
